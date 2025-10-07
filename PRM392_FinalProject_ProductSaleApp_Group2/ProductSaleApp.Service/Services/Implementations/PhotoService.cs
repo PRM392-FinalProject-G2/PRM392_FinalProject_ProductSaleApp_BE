f3f43de0d8953250f1e41d21efe33810a41cc468
Huynh Ngoc Hau <ngochau1310@gmail.com>
2025-10-07 18:20:10 +0700

    update db

---
+++ PRM392_FinalProject_ProductSaleApp_Group2/ProductSaleApp.Service/Services/Implementations/PhotoService.cs
@@ -0,0 +1,37 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using ProductSaleApp.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSaleApp.Service.Services.Implementations
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoService(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            if (file.Length <= 0) return null;
+
+            await using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = "prm_techzone"
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            return uploadResult.SecureUrl.ToString();
+        }
+    }
+}
