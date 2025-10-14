4fe2171d9048d60f6e0d3fc798b6437686b0b609
NgBaNhan <nhan23563@gmail.com>
2025-10-14 16:01:50 +0700

    Update Filter Product Feature

---
+++ PRM392_FinalProject_ProductSaleApp_Group2/ProductSaleApp.Service/BusinessModel/ProductFilterBM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSaleApp.Service.BusinessModel
+{
+    public class ProductFilterBM
+    {
+        public int? ProductId { get; set; }
+        public string? Search { get; set; }
+
+        public List<int>? CategoryIds { get; set; }
+        public List<int>? BrandIds { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        public double? AverageRating { get; set; }
+
+        // SortBy có thể là "Price", "Popularity", "Category"
+        public string? SortBy { get; set; }
+    }
+
+}
